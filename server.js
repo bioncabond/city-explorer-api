39a10e878c850a0d41d72932c90f76a736575edc
Bionca Bond <bioncab@zillowgroup.com>
2021-10-16 17:22:30 -0700

    lab08moviesback

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require ('axios');
-
-const weather = require('./data/weather.json');
-
 const app = express();
 app.use(cors());
 
@@ -17,6 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.get('/', (request, response) => response.status(200).send('This is the root. Nothing exciting here'));
 app.get('/weather', handleWeather);
 app.get('/weather', (request, response) => response.status(200).send('Weather is Working!'));
+app.get('/movies', handleMovies);
 
 
 
@@ -34,15 +32,10 @@ async function handleWeather(request, response) {
   let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=3&key=${process.env.WEATHER_API_KEY}`;
   //   let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=35.91&lon=31.95&days=3&key=25f65879afd747adbf7001ea154fe344`;
 
-
-  // console.log('foundCity:', foundCity);
-  // TEST: response.status(200).send('found city', foundCity.data);
-
   try{
     let weatherData = await axios.get(weatherURL);
     const weatherArray = weatherData.data.data.map((day) => new Forecast(day));
     response.status(200).send(weatherArray);
-
   }
   catch(error){
     console.log('cant find it');
@@ -52,11 +45,40 @@ async function handleWeather(request, response) {
 //   response.status(200).send('Weather here');
 }
 
+async function handleMovies(request,response){
+
+  let { cityName } = request.query;
+  let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
+  //   let movieURL = `https://api.themoviedb.org/3/search/company?api_key=8eae4e8ad57a8c6bc82691f8d0dce20d&query=seattle`;
+
+  try {
+    let movieData = await axios.get(movieURL);
+    console.log(movieData.data);
+    const moviesArray = movieData.data.results.map((movieList) => new Movies(movieList));
+    response.status(200).send(moviesArray);
+  }
+  catch (error){
+    response.status(404).send('Cant find that Movie');
+  }
+
+}
 
 class Forecast {
   constructor(day) {
     this.date = day.valid_date;
     this.description = `Low of ${day.low_temp}, high of ${day.max_temp}, with ${day.weather.description}`;
+  }
+}
+
+class Movies {
+  constructor(movieList) {
+    this.original_title = movieList.original_title;
+    this.overiview = movieList.overiview;
+    this.vote_average = movieList.vote_average;
+    this.vote_count = movieList.vote_count;
+    // this.image_url = movieList.image_url;
+    this.popularity = movieList.popularity;
+    this.release_date = movieList.release_date;
   }
 }
 
